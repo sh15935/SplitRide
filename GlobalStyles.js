5d944767a3d0839c0b682e08c0ab5dcb5bf474e0
safae <125465623+sh15935@users.noreply.github.com>
2024-06-10 03:34:13 +0100

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,45 @@
+/* fonts */
+export const FontFamily = {
+  interMedium: "Inter-Medium",
+  interBlack: "Inter-Black",
+};
+/* font sizes */
+export const FontSize = {
+  size_lg: 18,
+  size_3xs: 10,
+  size_3xl: 22,
+  size_11xl: 30,
+  size_xl: 20,
+};
+/* Colors */
+export const Color = {
+  colorWhitesmoke: "#f1f1f1",
+  colorGainsboro: "#d9d9d9",
+  colorWhite: "#fff",
+  colorBlack: "#000",
+  colorSilver: "#bcbaba",
+  colorGray_100: "#fffdfd",
+  colorGray_200: "#0b0b0b",
+  colorDarkgray_100: "#acabab",
+  colorDarkgray_200: "#a6a6a6",
+};
+/* Paddings */
+export const Padding = {
+  p_33xl: 52,
+  p_32xl: 51,
+  p_10xs: 3,
+  p_mini: 15,
+  p_11xs: 2,
+  p_sm: 14,
+  p_mid: 17,
+  p_smi: 13,
+  p_265xl: 284,
+};
+/* border radiuses */
+export const Border = {
+  br_9xl: 28,
+  br_16xl: 35,
+  br_17xl: 36,
+  br_6xl: 25,
+  br_xl: 20,
+};
